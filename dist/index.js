cbe150265c7bd47ad6129a8e43a9877d43ed2d43
Danila Osin <55761790+strange-cat-fe@users.noreply.github.com>
2022-03-15 16:01:58 +0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var ExEnum;
-(function (ExEnum) {
-    ExEnum[ExEnum["first"] = 0] = "first";
-    ExEnum[ExEnum["second"] = 1] = "second";
-})(ExEnum || (ExEnum = {}));
-console.log(Object.keys(ExEnum));
